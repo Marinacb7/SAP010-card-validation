b4605c048870113b8eabe0feb5613fc7d801dc80
Marinacb7 <marinacordeiro92@gmail.com>
2023-06-14 00:04:54 -0300

    Estilização

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -1,44 +1,60 @@
-// importamos o objeto `validator`, que contém as funções `isValid` e `maskify`
 import validator from '../src/validator';
 
+const assert = require('assert');
+
 describe('validator', () => {
-  it('debería ser un objeto', () => {
-    expect(typeof validator).toBe('object');
+  describe('isValid', () => {
+    it('should return true for a valid credit card number', () => {
+      const creditCardNumber = '45320151128336';
+
+      const result = validator.isValid(creditCardNumber);
+
+      assert.strictEqual(result, true);
+    });
+
+    it('should return false for an invalid credit card number', () => {
+      const creditCardNumber = '45320151128337';
+
+      const result = validator.isValid(creditCardNumber);
+
+      assert.strictEqual(result, false);
+    });
+
+    it('should return false for an empty credit card number', () => {
+      const creditCardNumber = '';
+
+      const result = validator.isValid(creditCardNumber);
+
+      assert.strictEqual(result, false);
+    });
   });
 
-  describe('validator.isValid', () => {
-    it('debería ser una función', () => {
-      expect(typeof validator.isValid).toBe('function');
+  describe('maskify', () => {
+    it('should mask the credit card number and keep the last four digits', () => {
+      const creditCardNumber = '45320151128336';
+      const expected = '##########8336';
+
+      const result = validator.maskify(creditCardNumber);
+
+      assert.strictEqual(result, expected);
     });
 
-    it('debería retornar true para "4083952015263"', () => {
-      expect(validator.isValid('4083952015263')).toBe(true);
+    it('should return an empty string for an empty credit card number', () => {
+      const creditCardNumber = '';
+      const expected = '';
+
+      const result = validator.maskify(creditCardNumber);
+
+      assert.strictEqual(result, expected);
     });
 
-    it('debería retornar true para "79927398713"', () => {
-      expect(validator.isValid('79927398713')).toBe(true);
-    });
+    it('should return the credit card number itself if it has less than 4 digits', () => {
+      const creditCardNumber = '123';
+      const expected = '123';
 
-    it('debería retornar false para "1234567890"', () => {
-      expect(validator.isValid('1234567890')).toBe(false);
-    });
-  });
+      const result = validator.maskify(creditCardNumber);
 
-  describe('validator.maskify', () => {
-    it('debería ser una función', () => {
-      expect(typeof validator.maskify).toBe('function');
-    });
-
-    it('Debería retornar "############5616" para "4556364607935616"', () => {
-      expect(validator.maskify('4556364607935616')).toBe('############5616');
-    });
-
-    it('Debería retornar "1" para "1"', () => {
-      expect(validator.maskify('1')).toBe('1');
-    });
-
-    it('Debería retornar "######orld" para "helloworld"', () => {
-      expect(validator.maskify('helloworld')).toBe('######orld');
+      assert.strictEqual(result, expected);
     });
   });
 });
